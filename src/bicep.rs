9055c1299f597b3580bb5f5074496f853be33224
robhendriks <cyanidenl@icloud.com>
2026-01-20 23:22:52 +0100

    fix(build): remove unused code

--- src/bicep.rs
+++ src/bicep.rs
@@ -1,13 +1,1 @@
-pub struct Project {
-    modules: Vec<Module>,
-}
 
-impl Project {
-    pub fn new() -> Self {
-        Project {
-            modules: Vec::new(),
-        }
-    }
-}
-
-pub struct Module {}
